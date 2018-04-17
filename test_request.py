06eec6b124ec667c027a30c13193a69f444329ba
Felix Su <felixs8696@berkeley.edu>
2018-04-17 04:19:14 +0000

    added test requestion script

---
+++ test_request.py
@@ -0,0 +1,29 @@
+import io
+import os
+import sys
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud.vision import types
+
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
+
+IMAGENET_PATH = '/home/felixsu/project/data/nips'
+IMG_ID = sys.argv[1]
+# The name of the image file to annotate
+file_name = os.path.join(os.path.join(IMAGENET_PATH, 'dev'), str(IMG_ID) + ".png")
+
+# Loads the image into memory
+with io.open(file_name, 'rb') as image_file:
+    content = image_file.read()
+
+image = types.Image(content=content)
+
+# Performs label detection on the image file
+response = client.label_detection(image=image, max_results=20)
+labels = response.label_annotations
+
+print('Labels:')
+for label in labels:
+    print(label.description)
